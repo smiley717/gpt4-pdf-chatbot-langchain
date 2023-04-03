27a9ad4d9238036842e4e597ac220e00a3765165
smiley717 <smiley.dev717@gmail.com>
2023-04-03 16:39:29 -0400

    chore: update the copywrite text.

--- pages/index.tsx
+++ pages/index.tsx
@@ -319,8 +319,8 @@ export default function Home() {
           </main>
         </div>
         <footer className="m-auto p-4">
-          <a href="https://twitter.com/mayowaoshin">
-            Powered by LangChainAI. Demo built by Mayo (Twitter: @mayowaoshin).
+          <a href="https://twitter.com/mongrelapp">
+            Powered by LangChainAI. Demo built by mongrelapp (Twitter: @mongrelapp).
           </a>
         </footer>
       </Layout>
